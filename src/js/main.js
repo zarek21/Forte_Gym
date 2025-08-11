b0706430074f9e00e82c32a54553e85442282e55
Jorge <jorge.saleme77@gmail.com>
2025-08-11 12:14:02 -0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// --- LÓGICA DE LA NAVEGACIÓN MÓVIL ---
+
+const headerElement = document.querySelector(".header");
+const mobileNavButton = document.querySelector(".header-mobile-btn");
+
+mobileNavButton.addEventListener("click", function () {
+  // 1. Añade/quita la clase que muestra/oculta el menú
+  headerElement.classList.toggle("header--nav-open");
+
+  // 2. Actualiza el atributo ARIA para accesibilidad
+  const isNavOpen = headerElement.classList.contains("header--nav-open");
+  mobileNavButton.setAttribute("aria-expanded", isNavOpen);
+});
